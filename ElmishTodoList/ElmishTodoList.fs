5778b28e89c20a4f125dc49acfbf6ba182d8aecc
Timothé Larivière <timothe.lariviere@outlook.com>
2018-07-13 17:24:06 +0200

    Item Page

--- ElmishTodoList/ElmishTodoList.fs
+++ ElmishTodoList/ElmishTodoList.fs
@@ -15,9 +15,12 @@ module App =
     type Model = 
         {
             Contacts: Contact list
+            SelectedContact: Contact option
+            Name: string
+            IsFavorite: bool
         }
 
-    type Msg = ToggleFavorite of Contact
+    type Msg = Select of Contact | SaveContact | ChangeName of string | ChangeIsFavorite of bool
 
     let initModel = 
         {
@@ -27,17 +30,23 @@ module App =
                     { Name = "Jim"; IsFavorite = true }
                     { Name = "John"; IsFavorite = false }
                     { Name = "Frank"; IsFavorite = true }
-                ]
+                ];
+            SelectedContact = None;
+            Name = ""
+            IsFavorite = false
         }
 
     let init () = initModel, Cmd.none
 
     let update msg model =
         match msg with
-        | ToggleFavorite contact ->
-            let newContact = { contact with IsFavorite = not contact.IsFavorite }
-            let newContacts = model.Contacts |> List.map (fun c -> if c = contact then newContact else c)
-            { model with Contacts = newContacts }, Cmd.none
+        | Select contact -> { model with SelectedContact = Some contact; Name = contact.Name; IsFavorite = contact.IsFavorite }, Cmd.none
+        | ChangeName name -> { model with Name = name }, Cmd.none
+        | ChangeIsFavorite isFavorite -> { model with IsFavorite = isFavorite }, Cmd.none
+        | SaveContact ->
+            let newContact = { model.SelectedContact.Value with Name = model.Name; IsFavorite = model.IsFavorite }
+            let newContacts = model.Contacts |> List.map (fun c -> if c = model.SelectedContact.Value then newContact else c)
+            { model with Contacts = newContacts; SelectedContact = None; Name = ""; IsFavorite = false }, Cmd.none
 
     let cellView name isFavorite =
         View.StackLayout(
@@ -49,21 +58,46 @@ module App =
         )
 
     let view (model: Model) dispatch =
-        View.ContentPage(
-            content=View.StackLayout(
-                children=
-                    [
-                        View.ListView(
-                            verticalOptions=LayoutOptions.FillAndExpand,
-                            itemTapped=(fun i -> model.Contacts.[i] |> ToggleFavorite |> dispatch),
-                            items=
-                                [
-                                    for contact in model.Contacts do
-                                        yield cellView contact.Name contact.IsFavorite
-                                ]
-                        )
+        let mainPage =
+            View.ContentPage(
+                title="My Contacts",
+                content=View.StackLayout(
+                    children=
+                        [
+                            View.ListView(
+                                verticalOptions=LayoutOptions.FillAndExpand,
+                                itemTapped=(fun i -> model.Contacts.[i] |> Select |> dispatch),
+                                items=
+                                    [
+                                        for contact in model.Contacts do
+                                            yield cellView contact.Name contact.IsFavorite
+                                    ]
+                            )
+                        ]
+                ) 
+            )
+
+        let itemPage =
+            View.ContentPage(
+                title="Item",
+                toolbarItems=[
+                    View.ToolbarItem(order=ToolbarItemOrder.Primary, text="Save", command=(fun() -> SaveContact |> dispatch))
+                ],
+                content=View.StackLayout(
+                    children=[
+                        View.Label(text="Name")
+                        View.Entry(text=model.Name, textChanged=(fun e -> e.NewTextValue |> ChangeName |> dispatch))
+                        View.Label(text="Is Favorite")
+                        View.Switch(isToggled=model.IsFavorite, toggled=(fun e -> e.Value |> ChangeIsFavorite |> dispatch))
                     ]
-            ) 
+                )
+            )
+
+        View.NavigationPage(
+            pages=
+                match model.SelectedContact with
+                | None -> [ mainPage ]
+                | Some _ -> [ mainPage; itemPage ]
         )
 
     // Note, this declaration is needed if you enable LiveUpdate
